b59a9643b7b102226a309b755db3b6010cd0f520
Wei <wei@Weis-MacBook-Pro.local>
2018-10-12 00:04:37 +1100

    feat: style Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,35 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-    return <Text>Albums!</Text>;
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>
+    );
+};
+
+// Styles
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 // Make the component available to others parts of the app
