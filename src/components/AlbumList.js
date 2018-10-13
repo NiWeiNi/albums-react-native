9de20e4263d5490ed1bd660bbdf3f3098bb04787
Wei <wei@Weis-MacBook-Pro.local>
2018-10-14 01:06:48 +1100

    feat: create AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+// Import library to make a component
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Component AlbumList
+const AlbumList = () => (
+    <View>
+        <Text>Album List!!</Text>
+    </View>
+);
+
+// Export component
+export default AlbumList;
