76d4bbca44a98598974378268b1f247b9e035980
Wei <wei@Weis-MacBook-Pro.local>
2018-10-14 01:46:37 +1100

    feat: fetch album data from server in AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,15 @@
 // Import library to make a component
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // Component AlbumList
 class AlbumList extends Component {
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response))
+    }
+
     render() {
         return (
             <View>
