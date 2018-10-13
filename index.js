373fa86330e810a66828b1ecec49761a8af16f30
Wei <wei@Weis-MacBook-Pro.local>
2018-10-14 01:08:24 +1100

    style: change header component name to Header

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import a library to help create a component
 import React from 'react';
 import { View, AppRegistry } from 'react-native';
-import Header from './src/components/header.js';
+import Header from './src/components/Header.js';
 import AlbumList from './src/components/AlbumList';
 
 // Create a component
