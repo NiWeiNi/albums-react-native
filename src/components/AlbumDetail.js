0a6453e1d9a9e36d0a3846348acf2174fe010b54
Wei <wei@Weis-MacBook-Pro.local>
2018-10-14 23:37:01 +1100

    feat: add Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = ( {album} ) => {
+const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
     const { 
         thumbnailStyle,
@@ -33,6 +34,9 @@ const AlbumDetail = ( {album} ) => {
                     style={imageStyle}
                     source={{ uri: image}}
                 />
+            </CardSection>
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
